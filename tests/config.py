b0bd5fd71dd7f75e07b902a49ba5f380a174b3f4
Noam Zimer <zimernoam@gmail.com>
2026-05-09 11:41:09 +0300

    Update tests to use Render URLs

--- tests/config.py
+++ tests/config.py
@@ -1,7 +1,7 @@
-LOGS_URL = "http://localhost:3003"
-USERS_URL = "http://localhost:3001"
-COSTS_URL = "http://localhost:3002"
-ABOUT_URL = "http://localhost:3004"
+LOGS_URL = "https://cost-manager-logs-service-02im.onrender.com"
+USERS_URL = "https://cost-manager-users-service-h3qu.onrender.com"
+COSTS_URL = "https://cost-manager-costs-service-673p.onrender.com"
+ABOUT_URL = "https://cost-manager-about-service-c2tt.onrender.com"
 
 TEST_USER_ID = 123123
 TEST_YEAR = 2026
